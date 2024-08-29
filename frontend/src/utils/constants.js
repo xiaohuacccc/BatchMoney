26260dcc469440e09a47b47f69c01ea843e0088a
xiaohuacccc <68153684+xiaohuacccc@users.noreply.github.com>
2024-08-29 22:13:45 +0800

    add story Chain

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,132 @@
+export const supportedChains = [
+  {
+    chainId: 1,
+    batchTransferAddress: "0xD35a289c1D5F6f6a604d6026111109694e51BA25",
+    blockExplorer: "https://etherscan.io/",
+    name: "eth mainnet",
+  },
+  {
+    chainId: 5,
+    batchTransferAddress: "0xDC7a1993196d63db926c3B2e1C42682f39885B96",
+    blockExplorer: "https://goerli.etherscan.io/",
+    name: "goerli",
+  },
+  {
+    chainId: 10,
+    batchTransferAddress: "0x3484593c456D9C598C47754341718062318066Ba",
+    blockExplorer: "https://optimistic.etherscan.io/",
+    name: "optimism",
+  },
+  {
+    chainId: 56,
+    batchTransferAddress: "0x0D69079B60484ae97EA7DEaad370B61f9Da401F8",
+    blockExplorer: "https://bscscan.com/",
+    name: "bnb chain",
+  },
+  {
+    chainId: 100,
+    batchTransferAddress: "0x1b285Ffd0B19805947E7F285A922d2fFe5c4FE4d",
+    blockExplorer: "https://gnosis.blockscout.com/",
+    name: "gnosis",
+  },
+  {
+    chainId: 137,
+    batchTransferAddress: "0x927ec65329636525a5B00103De1c00d8Da9b08aD",
+    blockExplorer: "https://polygonscan.com/",
+    name: "polygon",
+  },
+  {
+    chainId: 204,
+    batchTransferAddress: "0x1b285Ffd0B19805947E7F285A922d2fFe5c4FE4d",
+    blockExplorer: "https://opbnbscan.com/",
+    name: "opbnb",
+  },
+  {
+    chainId: 424,
+    batchTransferAddress: "0x1b285Ffd0B19805947E7F285A922d2fFe5c4FE4d",
+    blockExplorer: "https://explorer.publicgoods.network/",
+    name: "public goods network",
+  },
+  {
+    chainId: 1101,
+    batchTransferAddress: "0x1b285Ffd0B19805947E7F285A922d2fFe5c4FE4d",
+    blockExplorer: "https://zkevm.polygonscan.com/",
+    name: "polygon zkevm",
+  },
+  {
+    chainId: 8453,
+    batchTransferAddress: "0x1b285Ffd0B19805947E7F285A922d2fFe5c4FE4d",
+    blockExplorer: "https://basescan.org/",
+    name: "base",
+  },
+  {
+    chainId: 17000,
+    batchTransferAddress: "0x1b285Ffd0B19805947E7F285A922d2fFe5c4FE4d",
+    blockExplorer: "https://holesky.etherscan.io/",
+    name: "holesky",
+  },
+  {
+    chainId: 42161,
+    batchTransferAddress: "0x63d9C12865336322Ca981E5d1392acde4fAdD3Dc",
+    blockExplorer: "https://arbiscan.io/",
+    name: "arbitrum one",
+  },
+  {
+    chainId: 59144,
+    batchTransferAddress: "0x1b285Ffd0B19805947E7F285A922d2fFe5c4FE4d",
+    blockExplorer: "https://lineascan.build/",
+    name: "linea",
+  },
+  {
+    chainId: 534351,
+    batchTransferAddress: "0x1b285Ffd0B19805947E7F285A922d2fFe5c4FE4d",
+    blockExplorer: "https://sepolia-blockscout.scroll.io/",
+    name: "scroll sepolia",
+  },
+  {
+    chainId: 534352,
+    batchTransferAddress: "0x1b285Ffd0B19805947E7F285A922d2fFe5c4FE4d",
+    blockExplorer: "https://blockscout.scroll.io/",
+    name: "scroll",
+  },
+  {
+    chainId: 11155111,
+    batchTransferAddress: "0x800A2bE9B6259E252eDE4a5a041C23ab994F2962",
+    blockExplorer: "https://sepolia.etherscan.io/",
+    name: "sepolia",
+  },
+  {
+    chainId: 713715,
+    batchTransferAddress: "0x4CA7ac049D6E6344aCa8e50e76A2a136b15E451C",
+    blockExplorer: "https://seistream.app/transactions/",
+    name: "Sei EVM Devnet",
+  }, 
+  {
+    chainId: 4200,
+    batchTransferAddress: "0x27F8e467b47967080D7b2ba4CBE0457DBa6B9E43",
+    blockExplorer: "https://scan.merlinchain.io/",
+    name: "Merlin Mainnet",
+  },
+  {
+    chainId: 195,
+    batchTransferAddress: "0x982855F666E986AD835DDaD60673bb943C1cc939",
+    blockExplorer: "https://www.oklink.com/x1-test/",
+    name: "X1 Testnet",
+  },
+  {
+    chainId: 112358,
+    batchTransferAddress: "0xa5c288Ea7E391923607a0666e6e3Be34435C7708",
+    blockExplorer: "https://xterscan.io/",
+    name: "Xterio Chain",
+  },
+  {
+    chainId: 1513,
+    batchTransferAddress: "0x3139Aa4AcD178EFD8a49C8ac68416542fd7E2303",
+    blockExplorer: "https://testnet.storyscan.xyz/",
+    name: "story Chain",
+  },
+
+
+
+
+];
